ee317a481ffc876663fb31f6a818a4cd46488a90
Md. Noushin Islam <noushinshoha@gmail.com>
2024-09-25 09:37:05 +0600

    made changes to the history section

--- js/noakhali-donation.js
+++ js/noakhali-donation.js
@@ -23,7 +23,7 @@ document.getElementById('noakhali-donation-btn').addEventListener(
         history.innerHTML += `
             <div class="border-2 rounded-xl p-10 space-y-4 w-9/12">
                 <h4 class="font-bold">${donationValue} Taka is Donated for famine-2024 at Noakhali, Bangladesh</h4>
-                <p>Date: ${now.toString()}</p>
+                <p class="bg-slate-100 p-2 rounded-md text-sm">Date: ${now.toString()}</p>
             </div>
         ` 
 
